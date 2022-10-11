df5bffe2c16f7f3f8777099c887882d2b2ee0706
Maxim Fisenko <fisenko_maxim@mail.ru>
2022-10-11 16:17:06 +0300

    Добавлена страница  about_as, и файл гитигнор

--- Infomat/Clinica/views.py
+++ Infomat/Clinica/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+# You can work with Django Class.Watch dockumentation
 def index(request):
     return render(request, 'Clinica/index.html')
 
