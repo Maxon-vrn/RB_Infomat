dd3a8a31ef07c3decbd5cd0037e0209d2525959a
Maxim Fisenko <fisenko_maxim@mail.ru>
2022-10-12 18:50:30 +0300

    Сформирована структура дерева каталогов,подключен css,осуществлена сборка фотера

--- Infomat/Clinica/urls.py
+++ Infomat/Clinica/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('contact/', contact)
+    path('', index, name='index'),      #слайд шоу,6важных новостей с переходом ко всем новостям
+    path('news/', news, name='news'),       #здесь новости организованы по 5 шт по вертикали с погинацией
+    path('news/<slug:news>/', show_news, name='show_news'),     #заготовка под генерацию собственных страниц
+    path('contact/', contact, name='contact'),      #информация о юридическом адресе организации и контактах основных служб
+
 ]
--- Infomat/Clinica/views.py
+++ Infomat/Clinica/views.py
@@ -9,6 +9,13 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'Clinica/index.html')
 
+def news(request):
+    return render(request, 'Clinica/news.html')
 
+def show_news(request,news):
+    if request.GET:
+        print(request.GET)
+        print(request)
+    return HttpResponse(f"<h1>Pages to news on 1 news</h1><p>{news}</p>")
 def contact(request):
     return render(request, 'Clinica/contact.html')
