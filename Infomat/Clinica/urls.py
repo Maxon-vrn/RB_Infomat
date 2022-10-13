b997c44857b4cbf5970f6879c3b4c070c3cfc32d
Maxim Fisenko <fisenko_maxim@mail.ru>
2022-10-13 16:04:46 +0300

    Сайт запускается, но страница не отлажена.Нужно редактировать страницу news.html

--- Infomat/Clinica/urls.py
+++ Infomat/Clinica/urls.py
@@ -5,7 +5,7 @@ from django.views.i18n import JavaScriptCatalog     #для подключени
 
 urlpatterns = [
     path('', index, name='index'),      #слайд шоу,6важных новостей с переходом ко всем новостям
-    path('news/', AllPost.as_view(), name='news'),       #здесь новости организованы по 5 шт по вертикали с погинацией
+    path('news/', News.as_view(), name='news'),       #здесь новости организованы по 5 шт по вертикали с погинацией
     path('news/<slug:post>/', show_news, name='show_news'),     #заготовка под генерацию собственных страниц
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),  #после слова archive идет формирование свое страницы
     path('contact/', contact, name='contact'),      #информация о юридическом адресе организации и контактах основных служб
